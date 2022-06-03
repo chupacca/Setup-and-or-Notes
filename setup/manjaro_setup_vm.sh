c64d3893709013af4c6a75550b8b3ae9da69476e
*Pika <petachew@uw.edu>
2022-06-02 21:53:32 -0700

    Create manjaro_setup_vm.sh

---
+++ setup/manjaro_setup_vm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://techviewleo.com/install-vmware-tools-manjaro-arch-linux/
+# https://techviewleo.com/install-vmware-tools-manjaro-arch-linux/
+
+sudo pacman -Syyu
+sudo pacman -S open-vm-tools
+reboot
